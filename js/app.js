408cd19a9e5ebbee85d1be926643cd31bbf93029
BGil1011 <bryangil1112@hotmail.com>
2022-01-25 16:25:35 -0400

    main

---
+++ js/app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", inicio);
+
+function inicio(){
+    camisa();
+    producto(); 
+}
+
+
+function camisa(){
+    const camisa = document.querySelectorAll(".producto");
+
+    camisa.forEach((e)=>{
+        e.addEventListener("click", producto);
+    })
+}
+
+function producto(e){
+    e.preventDefault();
+    const ev = document.querySelector(".producto a")
+    const id = ev.parentElement.id;
+    const nombre = ev.children[1].children[0].textContent;
+    const precio = ev.children[1].children[1].textContent; 
+    const direccion = ev.href = "/producto.html";   
+    window.location.assign(`${direccion}?id=${id}&p=${precio}&n=${nombre}`);
+
+}
+
---
+++ js/app1.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", inicio);
+
+function inicio(){
+    cargarCamisa();
+}
+
+function cargarCamisa(){
+    var url = new URL(window.location);
+    const id = url.searchParams.get("id");
+    const nombre = url.searchParams.get("n");
+    const precio = url.searchParams.get("p");
+    const nombre_producto = document.querySelector(".contenedor h1");
+    const pagina = document.querySelector(".camisa");
+    const boton = document.querySelector(".formulario__submit");
+    boton.addEventListener("click", e=> e.preventDefault());
+    nombre_producto.textContent = nombre;
+    pagina.children[0].src = `img/${id}.jpg`;
+
+}
